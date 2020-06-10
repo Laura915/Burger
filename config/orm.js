5afaaca586e8f6844c7e5a6972d5f19a4ce45fd7
Laura <lhernan53112@gmail.com>
2020-06-10 11:14:12 -0400

    Created a insertOne method in orm object.

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,22 @@
 // ORM file, holds burger methods 
 const connection = require('../config/connection.js');
 
+// Helper function for SQL syntax.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+
 // Object for all our SQL methods.
 const orm = {
+    //selects all values method 
     selectAll: function (tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, function (err, result) {
@@ -11,7 +25,28 @@ const orm = {
             }
             cb(result);
         });
-    }
+    },
+    //InsertOne method that post data
+    insertOne:function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+      }
+
 
 }
 module.exports = orm;
