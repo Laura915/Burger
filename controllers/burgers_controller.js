899979693b8310bfbc1aed0853a33278fc0dcd6c
Laura <lhernan53112@gmail.com>
2020-06-10 15:23:22 -0400

    added post route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,22 +1,35 @@
 //router file with all unique routes
-const express= require('express');
-let router= express.Router();
+const express = require('express');
+let router = express.Router();
 
 //burger model
-let burger= require('../models/burger.js');
+let burger = require('../models/burger.js');
 
 // Routes 
 //get data route
-router.get("/",function(req, res) {
-    burger.selectAll(function(data) {
-      let hbsObject = {
-        burgers: data
-      };
-    //   console.log(hbsObject);
-      res.render("index", hbsObject);
-
-    });
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
+    let hbsObject = {
+      burgers: data
+    };
+    res.render("index", hbsObject);
   });
+});
+
+//post new data route
+router.post("/api/burger", function (req, res) {
+  burger.insertOne(
+    [
+      "burger_name", "devoured"
+    ],
+    [
+      req.body.burger_name, req.body.devoured
+    ], function (result) {
+      console.log(result);
+      res.json({ id: result.insertedID });
+    }
+  )
+})
 
 
-  module.exports=router;
+module.exports = router;
