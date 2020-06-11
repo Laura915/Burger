9b5ab3889a67b23cb127b6e434e3af0f053c8697
Laura <lhernan53112@gmail.com>
2020-06-11 13:26:31 -0400

    Added event listerner that will post new burger in the api

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -21,6 +21,28 @@ $(function(){
         });
     })
 
+    //Submit and Create new burger
+    $(".create-form").on("submit", function(event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+    
+        var newBurger = {
+          burger_name: $("#bu").val().trim(),
+        };
+    
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+        }).then(
+          function() {
+            console.log("created new cat");
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
+    
 
 
 
